bd046db05c63517b980d5c284f7a866758299d39
Victor Nguyễn <hoangloi669@gmail.com>
2020-08-13 10:01:53 +0700

    fix server crashing

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(compression);
 
 app.use(cors());
 
